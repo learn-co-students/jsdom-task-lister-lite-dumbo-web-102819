229b28e52d6c22cae59f9ec7a604f0b9834d7117
GAierken <g.aierken@gmail.com>
2019-12-10 14:25:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let toDoUl = document.getElementById("tasks");
+
+
+document.addEventListener("submit", function(evt){
+   evt.preventDefault()
+   let newLi = document.createElement("li")
+   let newTask = evt.target["new-task-description"].value
+      newLi.innerText = newTask
+      toDoUl.append(newLi)
+      console.log(toDoUl)
+
 });
